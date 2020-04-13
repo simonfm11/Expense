a913527bb8c214fbeaa23f923c687d1f31bf2b86
SIMON FLOREZ MONTES <simonflorez207934@correo.itm.edu.co>
2020-04-13 01:06:17 -0500

    Create the project common, prism , web and prism.android

---
+++ Expense.Web/Data/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expense.Web.Data.Entities
+{
+    public class UserEntity
+    {
+    }
+}
