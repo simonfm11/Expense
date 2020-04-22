1de0c51ef0ff3c8fd21b45bed3083289bf6df001
SIMON FLOREZ MONTES <simonflorez207934@correo.itm.edu.co>
2020-04-21 22:23:10 -0500

    Adding and edit corresponding details to tripresponse and tripdetailresponse

---
+++ Expense.Common/Models/TripDetailResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expense.Common.Models
+{
+    public class TripDetailResponse
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime StartDateLocal => Date.ToLocalTime();
+
+        public string PicturePath { get; set; }
+
+        public TripResponse Trip { get; set; }
+
+    }
+}
---
+++ Expense.Common/Models/TripResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expense.Common.Models
+{
+    public class TripResponse
+    {
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+        public string  Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        public DateTime EndDate { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        public UserResponse User { get; set; }
+
+        public ICollection<TripDetailResponse> TripDetails { get; set; }
+    }
+}
